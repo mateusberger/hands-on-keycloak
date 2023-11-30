8b12c3aae0caae39137db1877c35c76c64a40a2e
Mateus Berger <mateusberger96@gmail.com>
2023-11-30 15:57:25 -0300

    Feat: Criação do projeto para consumir o keycloak

---
+++ keycloakclient/src/main/java/com/mateus/keycloakclient/KeycloakclientApplication.java
@@ -0,0 +1,13 @@
+package com.mateus.keycloakclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakclientApplication.class, args);
+	}
+
+}
---
+++ keycloakclient/src/test/java/com/mateus/keycloakclient/KeycloakclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateus.keycloakclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
